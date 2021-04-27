01376c317052598f00b71dbf7e185e45c79f6c16
liuzhou <liuzhou@banggood.com>
2021-04-27 18:09:50 +0800

    #基础

--- Travel/src/pages/home/Home.vue
+++ Travel/src/pages/home/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home">
      <home-header></home-header>
   </div>
 </template>
--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="header">
-    <div class="header-item h-l"><span class="iconarrow-down-bold"></span></div>
-    <div class="header-item h-c">搜索</div>
-    <div class="header-item h-r">城市</div>
+     <div class="header-box h-b-r"><i class="iconfont iconarrow-left-bold"></i></div>
+     <div class="header-box h-b-c">2</div>
+     <div class="header-box h-b-r"><i class="iconfont iconarrow-down-filling"></i></div>
   </div>
 </template>
 
@@ -12,29 +12,10 @@ export default {
 }
 </script>
 
-<style lang='stylus' scoped>
+<style lang="stylus" scoped>
   .header
-    height .86rem
-    display flex
-    background #0bc4d0
-    align-items center
-    justify-items center
-    justify-content space-between
-    font-size .26rem
-    color #fff
-    text-align center
-    .h-l
-      width .64rem
-      font-size .32rem
-    .h-c
-      flex 1
-      background #fff
-      height .6rem
-      line-height .6rem
-      border-radius .05rem
-      color #aaa
-      padding 0 0.2rem
-      text-align left
-    .h-r
-      width 1.20rem
+   display flex
+   height .60rem
+   background #00bcd4
+   line-height 0.60rem
 </style>
